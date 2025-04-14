a9520718d0911a08e462df937640f7eb15332a1c
vaishnavvr13 <vaishnavvr13@gmail.com>
2025-04-14 15:34:13 +0100

    property crud

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const verifyToken = require('../middleware/verifyToken');
 const { register, login, logout } = require('../controllers/auth');
 const { adminDashboard, tenantDashboard, landlordDashboard } = require('../controllers/dashboard');
+const { createProperty, getProperties, updateProperty, deleteProperty, getAllProperties , createAdminProperty} = require('../controllers/property');
 const router = express.Router();
 
 router.post('/register', register);
@@ -12,6 +13,14 @@ router.get('/logout', logout);
 router.get('/admin-dashboard', verifyToken, adminDashboard);
 router.get('/tenant-dashboard', verifyToken, tenantDashboard);
 router.get('/landlord-dashboard', verifyToken, landlordDashboard);
+
+router.get('/manage-properties/view-property',verifyToken, getProperties);
+router.post('/manage-properties/add-property',verifyToken, createProperty);
+router.put('/manage-properties/update-property/:id', updateProperty);
+router.delete('/manage-properties/delete-property/:id', deleteProperty);
+
+router.get('/manage-properties/view-all-property',verifyToken, getAllProperties);
+router.post('/manage-properties/add-admin-property',verifyToken, createAdminProperty);
 
 module.exports = router;
 
